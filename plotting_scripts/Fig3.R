0951e4f420c449c503e68a8b1f4236b9d9650f44
Chang-Yu-Chang <ray57512@gmail.com>
2020-09-17 09:01:46 -0400

    Test leakage and generalist condition

--- plotting_scripts/Fig3.R
+++ plotting_scripts/Fig3.R
@@ -10,14 +10,51 @@ df_comp <- list.files("../data/raw/simulation/", full.names = T) %>%
     lapply(fread) %>%
     rbindlist()
 
-
-df_comp %>%
+#
+plot_temporal_comp <- function(df_comp) {
+    df_comp %>% 
     filter(Type == "consumer") %>% 
     mutate(ID = factor(ID)) %>% 
-    filter(Transfer >= 10)
-#    filter(Seed == 1, Well %in% paste0("W", 0:9)) %>% 
+    filter(Well %in% paste0("W", 0:19)) %>%
     ggplot() +
     geom_bar(aes(x = Transfer, y = Abundance, fill = ID), position = "stack", stat = "identity") +
     facet_wrap(Well~.) +
     guides(fill = F) +
     theme_bw()
+}
+plot_temporal_resource <- function(df_comp) {
+    df_comp %>% 
+        filter(Type == "resource") %>% 
+        mutate(ID = factor(ID)) %>% 
+        filter(Well %in% paste0("W", 0:19)) %>%
+        ggplot() +
+        geom_bar(aes(x = Transfer, y = Abundance, fill = ID), position = "stack", stat = "identity") +
+        facet_wrap(Well~.) +
+        guides(fill = F) +
+        theme_bw()
+}
+count_richness <- function(df_comp){
+    df_comp %>%
+        filter(Type == "consumer") %>% 
+        mutate(ID = factor(ID)) %>% 
+        filter(Transfer == max(Transfer)) %>% 
+        filter(Abundance >= 1e-6) %>% 
+        group_by(Well) %>%
+        summarize(Richness = n()) %>%
+        pull(Richness) %>% table()
+}
+
+df_comp <- fread("../data/raw/simulation/monoculture-random_isolates-1_composition.txt")
+df_comp <- fread("../data/raw/simulation/pair-random_isolates-1_composition.txt")
+
+plot_temporal(df_comp)
+count_richness(df_comp)
+
+
+plot_temporal_resource(df_comp)
+
+df_comp %>%
+    filter(Well == "W6") %>%
+    filter(Type == "resource")
+    
+        
